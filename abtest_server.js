58746c6f0c226684d957d740ae24e0f5da463651
Joris Röling <joris@bonboa.com>
2016-04-11 18:23:59 +0200

    v0.0.6

--- abtest_server.js
+++ abtest_server.js
@@ -1,13 +1,12 @@
-Roles._collection.find({roles:'admin'}).observeChanges({
+var roleCollection=Roles._collection?Roles._collection:Meteor.users;
+roleCollection.find({roles:'admin'}).observeChanges({
 
 	added: function(id,fields) {
-		// eyes({added:id,fields});
 		ABTestServer.adminIds = _.union(ABTestServer.adminIds?ABTestServer.adminIds:[],[fields.userId])
 	},
 
 	removed: function(id) {
-		Roles._collection.find({_id:id}).forEach(function(role) {
-			// eyes({removed:id});
+		roleCollection.find({_id:id}).forEach(function(role) {
 			ABTestServer.adminIds = _.without(ABTestServer.adminIds?ABTestServer.adminIds:[],[role.userId])
 		});
 	}
